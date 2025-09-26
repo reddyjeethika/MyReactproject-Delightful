d41f596b589e22394487ccfbee6790d4e0a9ca31
reddyjeethika <160132648+reddyjeethika@users.noreply.github.com>
2025-09-26 16:15:30 +0530

    Update Foodlist.js

--- src/Foodlist.js
+++ src/Foodlist.js
@@ -1,40 +1,40 @@
-export const vegItems = [ { id: 1, name: "Paneer Butter Masala", price: 2, image: "/images/paneer.jpg" },
-    { id: 2, name: "Dal Tadkaa", price: 2, image: "/images/DalTadka.jpg" },
-    { id: 3, name: "Veg Fried Rice", price: 1, image: "/images/VegFriedRice.jpg" },
-      { id: 4, name: "Puri", price: 3, image: "/images/Puri.jpg" },
-    { id: 5, name: "Potato Fry", price: 4, image: "/images/Potatofry.jpg" },
+export const vegItems = [ { id: 100, name: "Paneer Butter Masala", price: 2, image: "/images/paneer.jpg" },
+    { id: 2, name: "Dal Tadkaa", price: 20, image: "/images/DalTadka.jpg" },
+    { id: 3, name: "Veg Fried Rice", price: 100, image: "/images/VegFriedRice.jpg" },
+      { id: 4, name: "Puri", price: 30, image: "/images/Puri.jpg" },
+    { id: 5, name: "Potato Fry", price: 40, image: "/images/Potatofry.jpg" },
      { id: 6, name: "Paneer Butter Masala", price: 90, image: "/images/Paneer Biryani.jpg" },
-     { id: 7, name: "Paneer Curry", price: 4, image: "/images/paneer.jpg" },
+     { id: 7, name: "Paneer Curry", price: 40, image: "/images/paneer.jpg" },
      { id: 8, name: "Roti", price: 90, image: "/images/Roti.jpg" }
   ];
 
 
   export const nonVegItems = [ { id: 101, name: "Chicken Biryani", price: 1, image: "/images/chicken-biryani.jpg" },
-    { id: 102, name: "Mutton Curry", price: 2, image: "/images/Mutton Curry.jpg" },
-    { id: 103, name: "Fish Fry", price: 2, image: "/images/Fish fry.jpg" },
-    { id: 104, name: "Prawns Masala", price: 2, image: "/images/Prawns Curry.jpg" },
+    { id: 102, name: "Mutton Curry", price: 20, image: "/images/Mutton Curry.jpg" },
+    { id: 103, name: "Fish Fry", price: 20, image: "/images/Fish fry.jpg" },
+    { id: 104, name: "Prawns Masala", price: 20, image: "/images/Prawns Curry.jpg" },
     { id: 105, name: "Chicken 65 Curry", price: 160, image: "/images/chicken65 Curry.jpg" },
-    { id: 106, name: "Chicken Popcorn", price: 3, image: "/images/Chickenpopcorn.jpg" },
+    { id: 106, name: "Chicken Popcorn", price: 30, image: "/images/Chickenpopcorn.jpg" },
     { id: 107, name: "Egg Curry", price: 90, image: "/images/Egg Curry.jpg" },
     { id: 108, name: "Chilli Chicken", price: 160, image: "/images/Chilli Chicken.jpg" },
     { id: 109, name: "Butter Chicken", price: 140, image: "/images/Butter Chicken.jpg" }
   ];
   export const beverages = [
-      { id: 201, name: "Coffee", price: 2, image: "/Beverages/Coffee.jpg" },
-    { id: 202, name: "Mango Juice", price: 3, image: "/Beverages/mango-juice.jpg" },
+      { id: 201, name: "Coffee", price: 20, image: "/Beverages/Coffee.jpg" },
+    { id: 202, name: "Mango Juice", price: 30, image: "/Beverages/mango-juice.jpg" },
     { id: 203, name: "Lassi", price: 50, image: "/Beverages/Lassi.jpg" },
     { id: 204, name: "RoseMilk", price: 70, image: "/Beverages/RoseMilk.jpg" },
     { id: 205, name: "Tea", price: 30, image: "/Beverages/Tea.jpg" },
     { id: 206, name: "Pepsi", price: 50, image: "/Beverages/Pepsi.jpg" },
 
-    { id: 207, name: "Grape Juice", price: 3, image: "/Beverages/Grapejuice.jpeg" },
-    { id: 208, name: "Apple Juice", price: 30, image: "/Beverages/applejuice.jpg" },
+    { id: 207, name: "Grape Juice", price: 30, image: "/Beverages/Grapejuice.jpeg" },
+    { id: 208, name: "Apple Juice", price: 40, image: "/Beverages/applejuice.jpg" },
     { id: 209, name: "Mango Lassi", price: 30, image: "/Beverages/mangoLassi.jpg" }
   ];
   export const desserts = [
-     { id: 301, name: "Gulab Jamun", price: 4, image: "/Desserts/Gulabjamun.jpg" },
-    { id: 302, name: "Rasgulla", price: 2, image: "/Desserts/Rasgulla.jpg" },
-     { id: 303, name: "Strawberry Cake", price: 1, image: "/Desserts/Cake.jpg" },
+     { id: 301, name: "Gulab Jamun", price: 40, image: "/Desserts/Gulabjamun.jpg" },
+    { id: 302, name: "Rasgulla", price: 20, image: "/Desserts/Rasgulla.jpg" },
+     { id: 303, name: "Strawberry Cake", price: 100, image: "/Desserts/Cake.jpg" },
     
       { id: 304, name: "Caramel Pudding", price: 50, image: "/Desserts/pudding.jpg" },
        { id: 305, name: "Choclate Brownie", price: 50, image: "/Desserts/Brownies.jpg" },
